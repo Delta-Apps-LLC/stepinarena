8ac189b04701f3a185974a2c42d06f27748f6b03
Andrew <a.thibs98@gmail.com>
2024-11-20 22:22:34 -0700

    copying nuxt project to vuetify project

---
+++ src/layouts/default.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app>
+    <v-app-bar id="app-bar">
+      <v-app-bar-title>
+        <RouterLink to="/">
+          <img id="logo-btn" src="@/assets/images/title-logo-no-bg.png" />
+        </RouterLink>
+      </v-app-bar-title>
+      <v-spacer />
+
+      <div v-if="windowWidth > 800">
+        <RouterLink class="router-link"
+          v-for="(btn, i) in navBtns"
+          :key="i"
+          :to="btn.to"
+        >{{ btn.title }}</RouterLink>
+      </div>
+
+      <v-menu v-if="windowWidth <= 800">
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon size="30">mdi-menu</v-icon>
+          </v-btn>
+        </template> 
+
+        <v-list style="display: flex; flex-direction: column;">
+          <RouterLink class="router-link"
+            v-for="(btn, i) in navBtns"
+            :key="i"
+            :to="btn.to"
+          >{{ btn.title }}</RouterLink>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <RouterView />
+    </v-main>
+    
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import { RouterLink, RouterView, useRouter, useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue';
+
+const router = useRouter();
+const route = useRoute();
+const windowWidth = ref(0);
+
+onMounted(() => {
+  windowWidth.value = window.innerWidth;
+  window.addEventListener('resize', () => {
+    windowWidth.value = window.innerWidth;
+  });
+});
+
+const navBtns = [
+  { title: 'About', to: '/about' },
+  { title: 'Events', to: '/events' },
+  { title: 'Resources', to: '/resources' },
+  { title: 'Store', to: '/store' },
+  { title: 'Blog', to: '/blog' },
+  { title: 'Contact', to: '/contact' },
+];
+
+</script>
+
+<style scoped>
+@import '@/styles/style.css';
+
+#app-bar {
+  position: fixed;
+  padding: 6px;
+  display: flex;
+  flex-direction: row;
+  background-color: #D4C5B4;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 9999;
+}
+
+#logo-btn {
+  height: 50px;
+}
+
+</style>
---
+++ src/pages/resources.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="page">
+    <p class="big-header header-font">
+      Browse Our Resources
+    </p>
+    <p class="main-text text-block">{{ resourceIntro }}</p>
+
+    <div style="margin-top: 30px;"></div>
+
+    <p class="page-header header-font">
+      Suggested Books
+    </p>
+    <BookList />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+
+const resourceIntro = ref('We live in a world of mind-numbing technology and entertainment. Surrounding ourselves with positive and enlightening content helps us accomplish the mission of The Arena to improve our lives in meaningful ways. Please explore the books, podcasts, and other resources we have to offer!')
+
+</script>
+
+<style scoped>
+@import '@/styles/style.css';
+
+</style>
